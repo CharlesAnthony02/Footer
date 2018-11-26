747352113ea4211ec9772130e76c73770ea11a9a
charlesanthonyo2 <onyebuchicharles02@gmail.com>
2018-11-26 16:01:11 +0100

    i have commited

---
+++ Documents/fasthousing/fasthousing/resources/views/footer.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <footer class="container-fluid text-center">
+        <div class="row">
+            <div class="col-sm-4 border">
+               <h3>Latest Properties </h3>
+               <br>
+               <a>Houses</a>
+               <br>
+               <a>Shops</a>
+            </div>
+            <div class="col-sm-4 border">
+                <h3>Contact us </h3>
+                <p><a href="#" class="fa fa-phone"></a>08023571943</p>
+                <p><a href="#" class="far fa-envelope"></a>onyebuchicharles02@gmail.com<p>
+                <p><a href="#" class="fas fa-map-marker-alt"></a>62 ehi road by Asa Ochiriozuo house<p> 
+                
+            </div>
+            
+            <div class="col-sm-4 border">
+                <h3>Newsletter Signup</h3>
+                <br>
+                <p>Enter your e-mail address to get the latest news on fasthousing<p>
+                <br>
+                <input type="email" placeholder="Your e-mail" >   
+                <br>
+                <a href="#" class="fa fa-facebook"></a>
+                <a href="#" class="fa fa-twitter"></a>
+                <a href="#" class="far fa-envelope"></a>
+                <a href="#" class="fa fa-phone"></a>                                
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 last">
+                <p>Fasthousing @ 2018</p>
+            </div>
+        </div>
+        <style>
+        footer{
+            width:100%;
+            background-color: #23415c;
+            color: #fff;
+        
+            
+        }
+        .border{
+            border: 1px dashed #333;
+        }
+        . fa {
+            padding: 15px;
+            font-size: 25px;
+            color: #fff;
+        }
+        .fa:hover{
+            color:#d5d5d5;
+            text-decoration: none;
+        }
+        .last{
+            background-color:#23406c;
+            width: 100%;
+        }
+    </style>
+    </footer>  
+</body>
+</html>
